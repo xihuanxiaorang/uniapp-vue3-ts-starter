259750c04cd7607f59ace439f6bbbfce5ae00d2b
xiaorang <15019474951@163.com>
2024-06-30 17:09:59 +0800

    feat: 使用 wot-design-uni 组件库

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,44 +1,26 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">
-        {{ title }}
-      </text>
-    </view>
-  </view>
+  <wd-swiper :current="0" :list="swiperList" autoplay></wd-swiper>
+
+  <wd-grid :column="3" border>
+    <wd-grid-item icon="picture" text="文字" />
+    <wd-grid-item icon="picture" text="文字" />
+    <wd-grid-item icon="picture" text="文字" />
+    <wd-grid-item icon="picture" text="文字" />
+    <wd-grid-item icon="picture" text="文字" />
+    <wd-grid-item icon="picture" text="文字" />
+  </wd-grid>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-const title = ref('Hello')
+const swiperList = ref([
+  'https://unpkg.com/wot-design-uni-assets/redpanda.jpg',
+  'https://unpkg.com/wot-design-uni-assets/capybara.jpg',
+  'https://unpkg.com/wot-design-uni-assets/panda.jpg',
+  'https://img.yzcdn.cn/vant/cat.jpeg',
+  'https://unpkg.com/wot-design-uni-assets/meng.jpg',
+])
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  width: 200rpx;
-  height: 200rpx;
-  margin-top: 200rpx;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-  margin-left: auto;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
+<style lang="scss" scoped></style>
